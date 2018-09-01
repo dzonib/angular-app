0ed917c034b3278be9726a152a9a8c2c7cdda410
dzonib <nikolabosnjak381@gmail.com>
2018-09-01 08:23:43 +0200

    done with bascis

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -2,20 +2,29 @@ import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-server',
-  templateUrl: './server.component.html'
+  templateUrl: './server.component.html',
+  styles: [`
+    .online {
+      color: white;
+    }
+  `]
 })
 
 export class ServerComponent {
-  serverID: number = 10;
-  serverStatus: string = 'offline';
+  serverID;
+  serverStatus = 'offline';
 
-  getServerStatus(serverPort) {
-
-    if (serverPort === 4200) {
-      this.serverStatus = 'online';
-      return this.serverStatus;
-    }
-    
-    return this.serverStatus;
+  constructor() {
+    this.serverStatus = Math.floor(Math.random() * 10) > 5 ? 'offline' : 'online';
+    this.serverID = Math.floor((Math.random() * 4900 ) + 1000);
   }
+
+  getColor() {
+    return this.serverStatus === 'online' ? 'green' : 'red';
+  }
+
+  getServerStatus() {
+    return this.serverStatus === 'online' ? true : false;
+  }
+
 }
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,21 +1,32 @@
-import {Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
-@Component({selector: 'app-servers', templateUrl: './servers.component.html', styleUrls: ['./servers.component.css']})
+@Component({
+  selector: 'app-servers',
+  templateUrl: './servers.component.html',
+  styleUrls: ['./servers.component.css']
+})
 export class ServersComponent implements OnInit {
-
-  allowNewServer : boolean = false;
-
-  serverCreationStatus: string = 'No server was created' 
+  allowNewServer = false;
+  serverName = '';
+  serverCreated = false;
+  serverCreationStatus = 'No server was created';
+  servers = ['Testserver', 'Testserver2'];
 
   constructor() {
     setTimeout(() => {
       this.allowNewServer = true;
-    }, 2000)
+    }, 2000);
   }
 
   ngOnInit() {}
 
   onCreateServer() {
-    this.serverCreationStatus = 'Server was created'
+    this.serverCreated = true;
+    this.servers.push(this.serverName);
+    this.serverCreationStatus = `Server ${this.serverName} was created`;
+  }
+
+  onUpdateServerName(event: Event) {
+    this.serverName = (<HTMLInputElement>event.target).value;
   }
 }
